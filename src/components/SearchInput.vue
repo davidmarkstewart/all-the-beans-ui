f5a5a1702f9630815d9fbc69075e83c4ac7d3a66
David Stewart <davidmarkstewart@gmail.com>
2025-02-12 13:43:25 +0000

    refactored search and svg

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const props = defineProps<{
+  modelValue: string;
+}>();
+
+const emit = defineEmits(['update:modelValue']);
+
+const handleInput = (event: Event) => {
+  if (event.target) {
+    emit('update:modelValue', (event.target as HTMLInputElement).value);
+  }
+};
+</script>
+
+<template>
+  <label for="search" class="text-sm/6 font-medium text-gray-900">Search By Name</label>
+  <div class="mt-1 grid grid-cols-1">
+    <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+      <input
+        type="text"
+        name="search"
+        id="search"
+        :value="props.modelValue"
+        @input="handleInput"
+        class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+      />
+    </div>
+  </div>
+</template>
--- src/views/BeanList.vue
+++ src/views/BeanList.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import BeanListItem from '../components/BeanListItem.vue';
+import SearchInput from '../components/SearchInput.vue';
+import SelectArrowSvg from '../components/SelectArrowSvg.vue';
 import { useBeansStore } from '../stores/beans';
 import { uppercaseFirstAndRestLower } from '../helpers';
 import { ref, computed } from 'vue';
@@ -30,12 +32,6 @@ const filterOptions = computed(() => {
     costs: store.getFilteredBeansCostRanges(beans),
   };
 });
-
-const handleSearch = (event: Event) => {
-  if (event.target) {
-    search.value = (event.target as HTMLInputElement).value;
-  }
-};
 </script>
 
 <template>
@@ -47,9 +43,7 @@ const handleSearch = (event: Event) => {
           <option value="All">All</option>
           <option v-for="country in filterOptions.countries" :key="country" :value="country">{{ country }}</option>
         </select>
-        <svg class="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" data-slot="icon">
-          <path fill-rule="evenodd" d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
-        </svg>
+        <SelectArrowSvg />
       </div>
     </div>
     <div class="flex-1">
@@ -59,9 +53,7 @@ const handleSearch = (event: Event) => {
           <option value="All">All</option>
           <option v-for="colour in filterOptions.colours" :key="colour" :value="colour">{{ uppercaseFirstAndRestLower(colour) }}</option>
         </select>
-        <svg class="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" data-slot="icon">
-          <path fill-rule="evenodd" d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
-        </svg>
+        <SelectArrowSvg />
       </div>
     </div>
     <div class="flex-1">
@@ -71,18 +63,11 @@ const handleSearch = (event: Event) => {
           <option value="All">All</option>
           <option v-for="cost in filterOptions.costs" :key="cost.value" :value="cost.value">{{ cost.label }}</option>
         </select>
-        <svg class="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" data-slot="icon">
-          <path fill-rule="evenodd" d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
-        </svg>
+        <SelectArrowSvg />
       </div>
     </div>
     <div class="flex-1">
-      <label for="search" class="text-sm/6 font-medium text-gray-900">Search By Name</label>
-      <div class="mt-1 grid grid-cols-1">
-        <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-          <input type="text" name="search" id="search" @input="handleSearch"  class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" /> 
-        </div>
-      </div>
+      <SearchInput v-model="search" />
     </div>
   </div>
   <ul class="divide-y divide-[#1b09096f] bg-white rounded-lg shadow-lg">
